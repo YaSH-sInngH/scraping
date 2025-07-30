b1df7d1c27967ec1a2a9fcdb859942873271b278
Yash <singhyash0026@gmail.com>
2025-07-30 17:12:42 +0530

    commit for outdated

--- models/Product.js
+++ models/Product.js
@@ -6,17 +6,20 @@ const productSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
+
   price: {
     type: String,
     required: true,
     min: 0,
   },
+
   rating: {
     type: Number,
     min: 0,
     max: 5,
     default: 0,
   },
+
   url: {
     type: String,
     required: true,
@@ -27,12 +30,14 @@ const productSchema = new mongoose.Schema({
       message: props => `${props.value} is not a valid URL!`,
     }
   },
+
   category: {
     type: String,
     required: true,
     trim: true,
     index: true, // Optional: index for filtering
   },
+  
   scrapedAt: {
     type: Date,
     default: Date.now,
